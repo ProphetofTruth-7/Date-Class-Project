844ddafeb32173235b55169731028f35f4f86d05
Malakye Brown <prophetoftruth07@gmail.com>
2026-09-13 18:21:35 -0400

    Finally figured out the proper wrapping and constructor logic. Applied that, and the basic getter properties

--- Date.py
+++ Date.py
@@ -2,14 +2,18 @@ from datetime import date
 import calendar
 
 class Date:
-    def __init__(self, day: int = 1, month: int = 1, year: int = 1900):
-        self.day = day
-        self.month = month
-        self.year = year
+    def __init__(self, day: int = 1, month: int = 1, year: int = 1900) -> None:
+        self.__date = date(day, month, year)
 
-    def get_day(self):
-        return self.day
-    def get_month(self):
-        return self.month
-    def get_year(self):
-        return self.year
+
+    @property
+    def day(self) -> int:
+        return self.__date.day
+
+    @property
+    def month(self) -> int:
+        return self.__date.month
+
+    @property
+    def year(self) -> int:
+        return self.__date.year
