5d0a0faf2135b36d96da751ea22a11df3e52e860
Malakye Brown <prophetoftruth07@gmail.com>
2026-09-13 16:07:58 -0400

    Trying to make sense of the tasks assigned. I believe there is no use for a main.py or its equivalent, according to the instructions. In the process of removing it

--- Date.py
+++ Date.py
@@ -1,5 +1,15 @@
+from datetime import date
+import calendar
+
 class Date:
-    def __init__(self, day, month, year):
-        self.name = day
-        self.age = month
+    def __init__(self, day: int = 1, month: int = 1, year: int = 1900):
+        self.day = day
+        self.month = month
         self.year = year
+
+    def get_day(self):
+        return self.day
+    def get_month(self):
+        return self.month
+    def get_year(self):
+        return self.year
