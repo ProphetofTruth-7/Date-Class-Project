d7f7c830b5e67998c31055ac43f1806807b7cf82
Malakye Brown <prophetoftruth07@gmail.com>
2026-09-14 09:54:17 -0400

    Beginning Testing. Starting with the Default Constructor

--- Test_Date_Class.py
+++ Test_Date_Class.py
@@ -1,3 +1,9 @@
 import unittest
 from date_class import Date
 
+class TestDate(unittest.TestCase):
+    def test_default_constructor(self):
+        self.assertEqual(Date().alphabetic_return(), "January 01, 1900")
+
+if __name__ == "__main__":
+    unittest.main()
