eb9cab4e85859303e87d3436036b4a211b4faddf
Malakye Brown <prophetoftruth07@gmail.com>
2026-09-14 10:11:42 -0400

    First test ran and successfully passed

--- Test_Date_Class.py
+++ Test_Date_Class.py
@@ -3,7 +3,8 @@ from Date_Class import Date
 
 class TestDate(unittest.TestCase):
     def test_default_constructor(self):
-        self.assertEqual(Date().alphabetic_return(), "January 01, 1900")
+        self.assertEqual(Date().alphabetic_return(), "January 01, 1900", msg="Among Us")
+        """ Very simple. We run the constructor naturally and automatically. The assertEqual runs, checking if the value returned by alphabetic_return = what we expect/know the datetime value should be stored as"""
 
 if __name__ == "__main__":
     unittest.main()
