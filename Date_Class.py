ee87372660ab5a602fcab43febe460af090587ba
Malakye Brown <prophetoftruth07@gmail.com>
2026-09-13 21:58:05 -0400

    Comments finished

--- Date_Class.py
+++ Date_Class.py
@@ -4,43 +4,54 @@ import calendar
 class Date:
     def __init__(self, day: int = 1, month: int = 1, year: int = 1900) -> None:
         self.__date = date(day, month, year)
-    """ Constructor for the Date Class. Establishes the baseline values, but does not force validation. Importantly, does not initialize each value separately, keeping them firmly housed in datetime"""
+    """ Constructor for the Date Class. Establishes the baseline values, but does not force validation. Importantly, does not initialize each value separately, keeping them firmly housed in datetime """
 
     @property
     def day(self) -> int:
         return self.__date.day
+    """ Returns the day currently stored in datetime as a integer """
 
     @property
     def month(self) -> int:
         return self.__date.month
+    """ Returns the month currently stored in datetime as a integer """
 
     @property
     def year(self) -> int:
         return self.__date.year
+    """ Returns the year currently stored in datetime as a integer """
 
     def set_date(self, month: int, day: int, year: int) -> None:
         revised_date = date(year, month, day)
         self.__date = revised_date
+    """ Sets the date to the specified month, day, and year, creating a revised datetime object with said values """
 
     def is_leap_year(self) -> bool:
         return calendar.isleap(self.__date.year)
+    """ Checks if the currently stored year is a leap year, returning True if so """
 
     @staticmethod
     def is_leap_year(year: int) -> bool:
         return calendar.isleap(year)
+    """ Checks if the provided year is a leap year, returning True if so """
 
     def last_day(self) -> int:
         return calendar.monthrange(self.__date.year, self.__date.month)[1]
+    """ Returns the last day of the currently stored month(and year, if it matters) """
 
     @staticmethod
     def last_day(year: int, month: int) -> int:
         return calendar.monthrange(year, month)[1]
+    """ Returns the last day of the provided month(and year, if it matters) """
 
     def numeric_return(self) -> str:
         return self.__date.strftime("%m/%d/%Y")
+    """ Returns the data in numeric format(# Month/# Day/# Year) """
 
     def alphabetic_return(self) -> str:
         return self.__date.strftime("%B %d, %Y")
+    """ Returns the data in alphabetic format(Month Day, Year) """
 
     def alt_alphabetic_return(self) -> str:
         return self.__date.strftime("%d %B %Y")
+    """ Returns the data in an alternative alphabetic format(Day Month Year) """
