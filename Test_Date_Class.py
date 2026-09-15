cceb4eb44d0ae981ef4817d5b3f077f25be4a350
Malakye Brown <prophetoftruth07@gmail.com>
2026-09-14 22:59:49 -0400

    Realized that the report requested a specific date

--- Test_Date_Class.py
+++ Test_Date_Class.py
@@ -57,13 +57,16 @@ class TestDate(unittest.TestCase): #Allows unittest in. It then runs thorugh all
         self.assertEqual(self.date.last_day(2024, 2), 29)
 
     def test_numeric_return(self):
-        self.assertEqual(self.date.numeric_return(), "09/13/2026")
+        self.date.set_date(2021, 12, 25)
+        self.assertEqual(self.date.numeric_return(), "12/25/2021")
 
     def test_alphabetic_return(self):
-        self.assertEqual(self.date.alphabetic_return(), "September 13, 2026")
+        self.date.set_date(2021, 12, 25)
+        self.assertEqual(self.date.alphabetic_return(), "December 25, 2021")
 
-def test_alt_alphabetic_return(self):
-        self.assertEqual(self.date.alt_alphabetic_return(), "13, September 2026")
+    def test_alt_alphabetic_return(self):
+        self.date.set_date(2021, 12, 25)
+        self.assertEqual(self.date.alt_alphabetic_return(), "25 December 2021")
 
 if __name__ == "__main__":
     unittest.main()
